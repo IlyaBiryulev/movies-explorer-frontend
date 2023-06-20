f7d2dc82e3ffdd901389b8e446d8fbfe73f44ddf
Ilya Biryulev <birulevila@yandex.ru>
2023-06-21 00:25:47 +0300

    feat: сверстать страницу NotFound

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import './NotFound.css';
+
+import { Link } from "react-router-dom";
+
+function NotFound() {
+  return (
+    <body className='not-found'>
+      <h1 className='not-found__error'>404</h1>
+      <p className='not-found__error-text'>Страница не найдена</p>
+      <Link className="not-found__back" to="/">Назад</Link>
+    </body>
+  );
+}
+
+export default NotFound;
